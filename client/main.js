ac9226babdbbc80a42303ef2952f161030b531e1
shaief <shaief@gmail.com>
2014-11-13 15:42:50 +0200

    now pushes tenant and items to db :)

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ console.log("main.js");
 
 house = function(){
 	return Houses.findOne({users:{$in:[Meteor.userId()]}});
-}
+};
 
 Template.purchase.helpers({
 	users: function(){
@@ -18,14 +18,18 @@ Template.purchase.helpers({
 
 Template.purchase.events({
 	"click #button1id-0": function(JQevent, blazeTemplate){
+
+		var tenant = $("#tenant-names .tenant")
+			.filter(function(i, tenant){return tenant.selected;})
+			.map(function(i, tenant){return tenant.id})[0];
+
+		var items = $("#items-list").find(".single-item");
+		items = items.filter(function(i,item){return item.selected;})
+			.map(function(i, item){return item.id});
 		debugger;
-		var username = blazeTemplate.find('select').value;
-		var htmlUsername = blazeTemplate.find('select').item(username);
-		var userIdHtml = htmlUsername.attributes.getNamedItem('id');
-		var userId = userIdHtml.value;
-		var htmlItemsPurchased = blazeTemplate.find('select#category');
 		Purcheses.insert({
-			"user": userId
+			"tenant": tenant,
+			"items": items.toArray()
 		}); 
 	}
 });
